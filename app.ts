4ac877394802a5dd8d16546f8356bf60175aa218
reza.solghar@gmail.com <mras.arab@gmail.com>
2024-08-05 16:18:15 +0200

    express first request added

--- app.ts
+++ app.ts
@@ -1,7 +1,31 @@
-import ollama from 'ollama'
+import express from "express";
+import dotenv from "dotenv";
+import ollama from "ollama";
 
-const response = await ollama.chat({
-    model: 'phi3:mini',
-    messages: [{ role: 'system', content: 'Why is the sky blue?' }],
-  })
-  console.log(response)
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const getResponse = async () => {
+  const response = await ollama.chat({
+    model: "phi3:mini",
+    messages: [{ role: "user", content: "Why is the sky blue?" }],
+  });
+  return response.message.content;
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const content = await getResponse();
+    res.json({ message: content });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
